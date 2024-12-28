903b610ef4aaeb91d8d3a67e06ac44432a6f935b
Fede <federica.guiducci3@studio.unibo.it>
2024-12-28 16:47:49 +0100

    game state class and interface

--- src/main/java/it/unibo/model/GameState.java
+++ src/main/java/it/unibo/model/GameState.java
@@ -9,7 +9,50 @@ Lo stato del gioco (es. in pausa, in corso, vinto, perso). */
 
 
 package it.unibo.model;
+import it.unibo.model.interfaces.GameStateInterface;
 
-public class GameState {
-    
+public class GameState implements GameStateInterface {
+    private int score;
+    private int level;
+    private boolean isPaused;
+    private boolean isGameOver;
+
+    public GameState() {
+        this.score = 0;
+        this.level = 1;
+        this.isPaused = false;
+        this.isGameOver = false;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void addScore(int points) {
+        this.score += points;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void nextLevel() {
+        this.level++;
+    }
+
+    public boolean isPaused() {
+        return isPaused;
+    }
+
+    public void togglePause() {
+        this.isPaused = !this.isPaused;
+    }
+
+    public boolean isGameOver() {
+        return isGameOver;
+    }
+
+    public void setGameOver(boolean isGameOver) {
+        this.isGameOver = isGameOver;
+    }
 }
