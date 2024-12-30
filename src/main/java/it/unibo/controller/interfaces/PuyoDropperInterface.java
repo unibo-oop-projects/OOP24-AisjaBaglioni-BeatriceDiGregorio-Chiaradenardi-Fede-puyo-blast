259ab441fdadbad02665e6d942bdf338bc69c539
chiaradenardi <chiara.denardi2@studio.unibo.it>
2024-12-30 13:08:14 +0100

    puyo dropper interface

---
+++ src/main/java/it/unibo/controller/interfaces/PuyoDropperInterface.java
@@ -0,0 +1,27 @@
+//chiara
+package it.unibo.controller.interfaces;
+
+import it.unibo.model.Puyo;
+
+public interface PuyoDropperInterface{
+    /**
+     * Metodo per spawnare un nuovo Puyo e farlo cadere
+     */
+    void spawnAndDropPuyo();
+
+    /**
+     * Trova la prima posizione disponibile per un Puyo in una colonna
+     * 
+     * @param x La colonna in cui cercare
+     * @return La posizione Y disponibile, o -1 se non c'è spazio
+     */
+    int findAvailableYPosition(int x);
+
+    /**
+     * Logica di caduta di un Puyo
+     * 
+     * @param puyo Il Puyo che deve cadere
+     */
+    void dropPuyo(Puyo puyo);
+}
+
