99a48a12d7a730c4464b96c2974682a165424c20
Beatrice di Gregorio <beatrice.digregorio@studio.unibo.it>
2025-01-09 17:16:14 +0100

    added CannonModel interface

---
+++ src/main/java/it/unibo/model/interfaces/CannonModelInterface.java
@@ -0,0 +1,16 @@
+package it.unibo.model.interfaces;
+
+public interface CannonModelInterface {    
+    void moveLeft();
+
+    void moveRight();
+
+    int getX();
+
+    void aimUp();
+
+    void aimDown();
+
+    int getAngle();
+
+}
