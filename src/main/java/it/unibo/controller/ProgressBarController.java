ec20caea9584f7b43bf7f2a6301995de03ca7a3d
Beatrice di Gregorio <beatrice.digregorio@studio.unibo.it>
2025-01-10 19:36:33 +0100

    Create ProgressBarController implementation

---
+++ src/main/java/it/unibo/controller/ProgressBarController.java
@@ -0,0 +1,51 @@
+package it.unibo.controller;
+
+import it.unibo.model.ProgressBarModel;
+import it.unibo.view.ProgressBarView;
+import javax.swing.*;
+
+public class ProgressBarController {
+    private final ProgressBarModel progressBarModel;
+    private final ProgressBarView progressBarView;
+    private Timer progressBarTimer;
+
+    public ProgressBarController(ProgressBarView view){
+        this.progressBarModel = new ProgressBarModel();
+        this.progressBarView = view;
+
+        progressBarTimer = new Timer(250, e -> {
+            progressBarModel.incrementProgress(1);
+            progressBarView.setProgress(progressBarModel.getProgress());
+        });
+    }
+
+    public void startProgress() {
+        // Avvia il timer per l'aggiornamento della progress bar
+        progressBarTimer.start();
+    }
+
+    public void setProgress(int progress) {
+        progressBarModel.setProgress(progress); // Aggiorna il modello
+        progressBarView.setProgress(progressBarModel.getProgress()); // Aggiorna la vista
+    }
+
+    public void stopProgress(){
+        progressBarTimer.stop();
+    }
+
+    public void resetProgress(){
+        progressBarModel.resetCharge();
+        progressBarView.repaint();
+    }
+
+    public void resetProgressBar(){
+        stopProgress();
+        resetProgress();
+    }
+
+    public int getProgress() {
+        return progressBarModel.getProgress();
+    }
+
+
+}
