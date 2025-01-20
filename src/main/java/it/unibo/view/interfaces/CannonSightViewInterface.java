a21634b8a9a4c4e5e814cc920209c4d625ea7ebd
Beatrice di Gregorio <beatrice.digregorio@studio.unibo.it>
2025-01-20 17:54:31 +0100

    created CannonSightView interface

---
+++ src/main/java/it/unibo/view/interfaces/CannonSightViewInterface.java
@@ -0,0 +1,10 @@
+package it.unibo.view.interfaces;
+
+import java.awt.Graphics;
+
+public interface CannonSightViewInterface {
+    void draw(Graphics g);
+
+    void setCannonSightPosition(int newX, int newY);
+    
+} 
