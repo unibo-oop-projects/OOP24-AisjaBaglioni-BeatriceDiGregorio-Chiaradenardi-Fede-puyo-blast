ffcb338ba0d0f873bac28bedea829661b94f3f4a
Beatrice di Gregorio <beatrice.digregorio@studio.unibo.it>
2025-01-20 17:57:39 +0100

    created a CannonSightModel interface

---
+++ src/main/java/it/unibo/model/interfaces/CannonSightModelInterface.java
@@ -0,0 +1,15 @@
+package it.unibo.model.interfaces;
+
+public interface CannonSightModelInterface {
+    void moveLeft();
+
+    void moveRight();
+
+    int getX();
+
+    void moveUp();
+
+    void moveDown();
+
+    int getY();
+}
