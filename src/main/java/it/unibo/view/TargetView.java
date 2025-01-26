ac2ac1cca03032bee34d249ec9cfb60fc748448e
Beatrice di Gregorio <beatrice.digregorio@studio.unibo.it>
2025-01-26 21:24:11 +0100

    Integrated `CannonModel` to dynamically position the target based on cannon state, refactored the draw method to calculate and render the target position relative to the cannon and improve image loading logic

--- src/main/java/it/unibo/view/TargetView.java
+++ src/main/java/it/unibo/view/TargetView.java
@@ -2,38 +2,57 @@ package it.unibo.view;
 
 import javax.swing.*;
 
+import it.unibo.model.CannonModel;
+import it.unibo.model.Scale;
 import it.unibo.view.interfaces.TargetViewInterface;
 
 import java.awt.*;
 import java.net.URL;
 
 public class TargetView extends JPanel implements TargetViewInterface {
-    
-    private Image cannonSightImage;
-    private int cannonSightX;
-    private int cannonSightY;
 
-    public TargetView(final String imagePath){
-        final URL imageUrl = getClass().getClassLoader().getResource("images/"+ imagePath);
+    private final Image cannonTargetImage;
+    private int imageWidth;
+    private int imageHeight;
+    private final Scale scale;
+    private final CannonModel cannonModel;
 
-        if (imageUrl == null){
+    public TargetView(String imagePath, Scale scale, CannonModel cannonModel) {
+        this.scale = scale;
+        this.cannonModel = cannonModel;
+        final URL imageUrl = getClass().getClassLoader().getResource("images/" + imagePath);
+
+        if (imageUrl == null) {
             System.err.println("Immagine non trovata: " + imagePath);
-            return;
+            throw new RuntimeException("Immagine non trovata: " + imagePath);
         }
 
-        this.cannonSightImage = new ImageIcon(imageUrl).getImage();
-        this.cannonSightX = 0;
-        this.cannonSightY = 0;
+        this.cannonTargetImage = new ImageIcon(imageUrl).getImage();
+        this.imageWidth = cannonTargetImage.getWidth(null);
+        this.imageHeight = cannonTargetImage.getHeight(null);
     }
 
     @Override
-    final public void draw(final Graphics g){
-        g.drawImage(cannonSightImage, cannonSightX, cannonSightY, null);
-    }
-
-    @Override
-    final public void setCannonSightPosition(final int newX, final int newY){
-        this.cannonSightX = newX;
-        this.cannonSightY = newY;
+    final public void draw(final Graphics g) {
+        int puyoCellSize = this.scale.getScale() / 16;
+        int width = this.scale.getScale() / 10;
+        int height = (width * this.imageHeight) / this.imageWidth;
+        int offset_gridx = puyoCellSize * 4;
+        int offset_gridy = puyoCellSize;
+        int grid_width = puyoCellSize * 8;
+        int grid_height = puyoCellSize * 8;
+        int cannonPosX = offset_gridx + ((int) (cannonModel.getX() * grid_width));
+        int cannonPosY = offset_gridy + ((int) ((1.0 - cannonModel.getAngle()) * grid_height));
+        g.drawImage(
+                cannonTargetImage,
+                cannonPosX - width / 2,
+                cannonPosY - height / 2,
+                cannonPosX + width / 2,
+                cannonPosY + height / 2,
+                0,
+                0,
+                imageWidth,
+                imageHeight,
+                null);
     }
 }
