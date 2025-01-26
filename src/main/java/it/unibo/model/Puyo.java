27f9b7f43523773598ec72082b10829200924f5d
Fede <federica.guiducci3@studio.unibo.it>
2025-01-26 23:16:41 +0100

    fix some bugs in puyo class

--- src/main/java/it/unibo/model/Puyo.java
+++ src/main/java/it/unibo/model/Puyo.java
@@ -2,7 +2,6 @@
 
 package it.unibo.model;
 
-import it.unibo.model.interfaces.GridInterface;
 import it.unibo.model.interfaces.PuyoInterface;
 
 import java.util.Optional;
@@ -13,9 +12,9 @@ public class Puyo implements PuyoInterface {
     private String color;
     private int x;
     private int y;
-    private boolean isFalling;
+
     Optional<Integer> deathClock;
-    Optional<Integer> freezeClock;
+    //Optional<Integer> freezeClock;
 
     // Costruttore
     public Puyo(String color, int x, int y) {
@@ -23,26 +22,29 @@ public class Puyo implements PuyoInterface {
         this.x = x;
         this.y = y;
         this.identifier = Math.abs(ThreadLocalRandom.current().nextLong());
-        this.isFalling = true;
+        
         deathClock=Optional.empty();
-        freezeClock=Optional.empty();
+        //freezeClock=Optional.empty();
+    }
+
+    public Optional<Integer> getDeathClock(){
+        return deathClock;
     }
 
     public void setDeathClock(Optional<Integer> deathClock){
         this.deathClock = deathClock;
     }
 
+    /*
     public void setFreezeClock(Optional<Integer> freezeClock){
         this.freezeClock = freezeClock;
-    }
+    }*/
 
-    public Optional<Integer> getDeathClock(){
-        return this.deathClock;
-    }
-
+    
+    /*
     public Optional<Integer> getFreezeClock(){
         return this.freezeClock;
-    }
+    }*/
 
     public long getIdentifier() {
         return identifier;
@@ -78,47 +80,9 @@ public class Puyo implements PuyoInterface {
         this.y = y;
     }
 
-    @Override
-    public boolean isFalling() {
-        return isFalling;
-    }
-
-    @Override
-    public void setFalling(boolean isFalling) {
-        this.isFalling = isFalling;
-    }
 
     @Override
     public void moveDown() {
-        if (isFalling) {
-            y += 1; // Incrementa la posizione verticale
-        }
-    }
-
-    @Override
-    public boolean checkCollision(GridInterface grid) {
-        // Controlla se il Puyo ha raggiunto il fondo o se c'è un altro Puyo sotto
-        if (!grid.isValidPosition(x, y + 1) || grid.getPuyo(x, y + 1) != null) {
-            isFalling = false; // Ferma il movimento del Puyo
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void explode() {
-        System.out.println("Puyo at (" + x + ", " + y + ") exploded!");
-    }
-
-    @Override
-    public void updateGrid(GridInterface grid) {
-        // Rimuovi il Puyo dalla posizione corrente nella griglia
-        grid.removePuyo(x, y);
-
-        // Se il Puyo è in caduta, aggiornalo nella nuova posizione
-        if (isFalling && grid.isValidPosition(x, y + 1)) {
-            y += 1; // Aggiorna la posizione del Puyo
-            grid.addPuyo(this, x, y);
-        }
+        y +=1;    
     }
 }
