7ce34455ff10bd947d9de3962f182fccb1e8417d
Fede <federica.guiducci3@studio.unibo.it>
2025-01-28 22:40:57 +0100

    start model master class

--- src/main/java/it/unibo/model/ModelMaster.java
+++ src/main/java/it/unibo/model/ModelMaster.java
@@ -1,5 +1,43 @@
 package it.unibo.model;
 
-public class ModelMaster {
+import it.unibo.model.interfaces.ModelMasterInterface;
+
+public class ModelMaster implements ModelMasterInterface{
+
+    @Override
+    public Grid getGrid() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getGrid'");
+    }
+
+    @Override
+    public CannonModel getCannonModel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getCannonModel'");
+    }
+
+    @Override
+    public ProgressBarModel getProgressBarModel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProgressBarModel'");
+    }
+
+    @Override
+    public BulletModel getBulletModel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBulletModel'");
+    }
+
+    @Override
+    public KeyboardModel getKeyboardModel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getKeyboardModel'");
+    }
+
+    @Override
+    public PauseModel getPauseModel() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPauseModel'");
+    }
     
 }
