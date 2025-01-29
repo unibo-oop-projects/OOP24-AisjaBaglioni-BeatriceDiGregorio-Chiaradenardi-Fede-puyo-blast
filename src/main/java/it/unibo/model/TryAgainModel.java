0dea376036bb5b6cef58166eecd8ee5471d42154
chiaradenardi <chiara.denardi2@studio.unibo.it>
2025-01-29 15:40:21 +0100

    tryagainmodel

--- src/main/java/it/unibo/model/TryAgainModel.java
+++ src/main/java/it/unibo/model/TryAgainModel.java
@@ -1,5 +1,18 @@
 package it.unibo.model;
 
 public class TryAgainModel {
-    
+    private boolean isEnabled;
+
+    public TryAgainModel() {
+        this.isEnabled = true;
+    }
+
+    public boolean isEnabled() {
+        return isEnabled;
+    }
+
+    public void setEnabled(boolean enabled) {
+        this.isEnabled = enabled;
+    }
 }
+
