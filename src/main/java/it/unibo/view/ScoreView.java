f6ee42e2c90f6f5b572987bc3e46ba4bf8db154d
Beatrice di Gregorio <beatrice.digregorio@studio.unibo.it>
2025-01-29 22:43:50 +0100

    Update score print

--- src/main/java/it/unibo/view/ScoreView.java
+++ src/main/java/it/unibo/view/ScoreView.java
@@ -2,9 +2,10 @@ package it.unibo.view;
 
 import it.unibo.model.ScoreModel;
 import it.unibo.view.interfaces.ScoreViewInterface;
-import javafx.scene.text.Font;
+//import javafx.scene.text.Font;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 
 import it.unibo.model.Scale;
@@ -20,10 +21,17 @@ public class ScoreView{
 
     final public void draw(Graphics g) {
         int cellsize = this.scale.getScale()/16;
-        int x = this.scale.getScale() - 2*cellsize;
-        int y = this.scale.getScale() - cellsize;
+        int x = this.scale.getScale() - 3*cellsize;
+        int y = this.scale.getScale() - 2*cellsize;
+
+        g.setFont(new Font("Ariel", Font.BOLD, cellsize/3));
+
+        g.setColor(Color.WHITE);
+        g.drawString("Score: " + this.score.getScore(), x + 1, y + 1);
+
         g.setColor(Color.BLACK);
         g.drawString("Score: " + this.score.getScore(), x, y);
+       
     }
 
 }
