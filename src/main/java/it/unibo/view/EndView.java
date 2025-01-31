9e52c0a63a63c03ee3d2d4e4ee011ba13d9dd918
Beatrice di Gregorio <beatrice.digregorio@studio.unibo.it>
2025-01-31 19:13:25 +0100

    Added initial view prototype for the end

---
+++ src/main/java/it/unibo/view/EndView.java
@@ -0,0 +1,16 @@
+package it.unibo.view;
+
+import it.unibo.model.Scale;
+import it.unibo.model.StatusModel;
+
+public class EndView {
+    private final StatusModel statusModel;
+    private final Scale scale;
+
+    public EndView(StatusModel statusModel, Scale scale) {
+        this.statusModel = statusModel;
+        this.scale = scale;
+    }
+
+    
+}
