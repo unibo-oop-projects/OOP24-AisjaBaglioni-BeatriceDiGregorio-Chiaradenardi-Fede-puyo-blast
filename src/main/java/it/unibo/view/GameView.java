3fe5b150720e9cb513470473c46f70ebaafafebb
Fede <federica.guiducci3@studio.unibo.it>
2025-02-01 19:20:31 +0100

    fix game view

--- src/main/java/it/unibo/view/GameView.java
+++ src/main/java/it/unibo/view/GameView.java
@@ -9,7 +9,6 @@ import it.unibo.view.interfaces.GameViewInterface;
 import it.unibo.view.interfaces.ClickInterface;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.util.HashSet;
 import java.util.Set;
