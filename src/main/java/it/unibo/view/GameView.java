fd4faca8a5ed4d9d8d602a2546ce017d450e8a8d
Fede <federica.guiducci3@studio.unibo.it>
2025-02-04 19:40:19 +0100

    update game view

--- src/main/java/it/unibo/view/GameView.java
+++ src/main/java/it/unibo/view/GameView.java
@@ -1,61 +1,42 @@
 //fede
 package it.unibo.view;
 
-import it.unibo.controller.ClickController;
-import it.unibo.controller.ScreenManager;
+import it.unibo.controller.ControllerStorage;
 import it.unibo.model.*;
 import it.unibo.model.interfaces.PuyoInterface;
-import it.unibo.view.interfaces.GameViewInterface;
-import it.unibo.view.interfaces.ClickInterface;
 import javax.swing.*;
 import java.awt.*;
-import java.util.HashSet;
-import java.util.Set;
 
-public class GameView extends JPanel implements GameViewInterface {
-    private ClickController clickController;
-    private Background background;
-    private final PuyoRenderer renderer;
+public class GameView extends JPanel {
+    public final Background background;
+    public final PuyoRenderer puyoRenderer;
+    public final CannonView cannonView;
+    public final TargetView cannonSightView;
+    public final ProgressBarView progressBarView;
+    public final BulletView bulletView;
+    public final BorderView borderView;
+    public final PauseView pauseView;
+    public final ExitView exitView;
+    public final TryAgainView tryAgainView;
+    public final ScoreView scoreView;
+    public final EndView endView;
     private final Grid grid;
-    private final CannonView cannonView;
-    private final TargetView cannonSightView;
-    private final ProgressBarView progressBarView;
-    private final BulletView bulletView;
-    private final BorderView borderView;
-    private final PauseView pauseView;
-    private final ExitView exitView;
-    private final TryAgainView tryAgainView;
-    private final ScoreView scoreView;
-    private final EndView endView;
-    private Scale scale;
-    private Set<ClickInterface> clickables;
 
-    public GameView(Grid grid, Scale scale, PuyoRenderer puyoRenderer, CannonModel cannonModel, CannonView cannonView,
-            ProgressBarModel progressModel, BulletModel bulletModel,
-            PauseView pauseView, TryAgainView tryAgainView,
-            ExitView exitView, ScoreView scoreView, EndView endView, ScreenManager screenManager) {
-
-        this.scale = scale;
-        this.pauseView = pauseView;
-        this.exitView = exitView;
-        this.tryAgainView = tryAgainView;
-        this.clickables = new HashSet<>();
-        this.clickables.add(this.exitView);
-        this.clickables.add(this.tryAgainView);
-        this.clickables.add(this.pauseView);
-        this.clickController = new ClickController(clickables);
+    public GameView(ModelStorage modelStorage, ControllerStorage controllerStorage) {
+        this.grid = modelStorage.grid;
+        this.cannonView = new CannonView(modelStorage.scale, modelStorage.cannonModel);
+        this.pauseView = new PauseView(modelStorage.scale, modelStorage.pauseModel, controllerStorage.pauseController);
+        this.exitView = new ExitView(modelStorage.scale, controllerStorage.exitController);
+        this.tryAgainView = new TryAgainView(modelStorage.scale, controllerStorage.tryAgainController);
         this.background = new Background("background.jpg");
-        this.renderer = puyoRenderer;
-        this.cannonView = cannonView;
-        this.cannonSightView = new TargetView("CannonSightView.png", this.scale, cannonModel);
-        this.progressBarView = new ProgressBarView(progressModel, this.scale);
-        this.bulletView = new BulletView(bulletModel, this.scale);
-        this.borderView = new BorderView(this.scale);
-        this.scoreView = scoreView;
-        this.endView = endView;
-        this.grid = grid;
+        this.puyoRenderer = new PuyoRenderer(modelStorage.scale);
+        this.cannonSightView = new TargetView(modelStorage.scale, modelStorage.cannonModel);
+        this.progressBarView = new ProgressBarView(modelStorage.progressBarModel, modelStorage.scale);
+        this.bulletView = new BulletView(modelStorage.bulletModel, modelStorage.scale);
+        this.borderView = new BorderView(modelStorage.scale);
+        this.scoreView = new ScoreView(modelStorage.scoreModel, modelStorage.scale);
+        this.endView = new EndView(modelStorage.statusModel, modelStorage.scale, modelStorage.scoreModel);
 
-        this.addMouseListener(clickController);
         this.setFocusable(true);
         this.requestFocusInWindow();
     }
@@ -69,7 +50,7 @@ public class GameView extends JPanel implements GameViewInterface {
             for (int x = 0; x < grid.getCols(); x++) {
                 PuyoInterface puyo = grid.getPuyo(x, y);
                 if (puyo != null) {
-                    renderer.render(g, grid, y, x);
+                    puyoRenderer.render(g, grid, y, x);
                 }
             }
         }
@@ -88,38 +69,8 @@ public class GameView extends JPanel implements GameViewInterface {
 
     }
 
-    /*
-     * private void drawPauseMessage(Graphics g) {
-     * String pauseText = "Game Paused";
-     * g.setFont(new Font("Arial", Font.BOLD, 36));
-     * g.setColor(Color.RED);
-     * FontMetrics metrics = g.getFontMetrics();
-     * int x = (getWidth() - metrics.stringWidth(pauseText)) / 2;
-     * int y = getHeight() / 2;
-     * g.drawString(pauseText, x, y);
-     * }
-     */
-
-    public CannonView getCannonView() {
-        return this.cannonView;
-    }
-
-    public TargetView getCannonSightView() {
-        return this.cannonSightView;
-    }
-
-    public ProgressBarView getProgressBarView() {
-        return this.progressBarView;
-    }
-
-    @Override
     public void render(Graphics g, int width, int height) {
         background.draw(g, width, height);
-    }
-
-    @Override
-    public void startGame() {
-        System.out.println("Game started!");
     }
 
 }
