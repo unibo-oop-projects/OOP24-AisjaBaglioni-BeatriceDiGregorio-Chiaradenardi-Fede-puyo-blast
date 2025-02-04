325048d0280d9a65fd265bf12e8845e504fe2358
chiaradenardi <chiara.denardi2@studio.unibo.it>
2025-02-04 20:21:18 +0100

    exit interface

--- src/main/java/it/unibo/model/ExitModel.java
+++ src/main/java/it/unibo/model/ExitModel.java
@@ -1,16 +1,20 @@
 package it.unibo.model;
 
-public class ExitModel {
+import it.unibo.model.interfaces.ExitModelInterface;
+
+public class ExitModel implements ExitModelInterface{
     private boolean exitClicked;
 
     public ExitModel() {
         this.exitClicked = false;
     }
 
+    @Override
     public boolean isExitClicked() {
         return exitClicked;
     }
 
+    @Override
     public void setExitClicked(boolean exitClicked) {
         this.exitClicked = exitClicked;
     }
---
+++ src/main/java/it/unibo/model/interfaces/ExitModelInterface.java
@@ -0,0 +1,11 @@
+package it.unibo.model.interfaces;
+
+
+public interface ExitModelInterface {
+
+    boolean isExitClicked();
+
+    void setExitClicked(boolean exitClicked);
+
+}
+
