008915b80e76c9f533403178f1d3f87ed635acbf
Aisja Baglioni <aisjabaglioni@gmail.com>
2025-02-04 20:43:16 +0100

    rectangle interface

--- src/main/java/it/unibo/model/Rectangle.java
+++ src/main/java/it/unibo/model/Rectangle.java
@@ -1,6 +1,8 @@
 package it.unibo.model;
 
-public class Rectangle {
+import it.unibo.model.interfaces.RectangleInterface;
+
+public class Rectangle implements RectangleInterface {
     public Point2DI upleft;
     public Point2DI lowright;
 
@@ -9,6 +11,7 @@ public class Rectangle {
         this.lowright = lowright;
     }
 
+    @Override
     public boolean isInside(Point2DI pos) {
         return (pos.x() >= upleft.x() && pos.x() <= this.lowright.x() && pos.y() <= this.lowright.y()
                 && pos.y() >= this.upleft.y());
---
+++ src/main/java/it/unibo/model/interfaces/RectangleInterface.java
@@ -0,0 +1,7 @@
+package it.unibo.model.interfaces;
+
+import it.unibo.model.Point2DI;
+
+public interface RectangleInterface {
+    boolean isInside(Point2DI pos);
+}
