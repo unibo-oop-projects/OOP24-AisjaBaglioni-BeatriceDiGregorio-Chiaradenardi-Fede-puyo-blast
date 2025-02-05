7d4e2156300e85ef3db31e91a808cd5fa9e663eb
Aisja Baglioni <aisjabaglioni@gmail.com>
2025-02-05 18:11:28 +0100

    scale comments

--- src/main/java/it/unibo/model/Scale.java
+++ src/main/java/it/unibo/model/Scale.java
@@ -4,17 +4,37 @@ package it.unibo.model;
 
 import it.unibo.model.interfaces.ScaleInterface;
 
-public class Scale implements ScaleInterface{
+/**
+ * A utility class representing the size of the window,
+ * being the unit of measurement adopted by all the views
+ */
+public class Scale implements ScaleInterface {
+    /*
+     * A single integer value, since the window is a square
+     */
     private int scale;
 
+    /**
+     * Constructs a new scale
+     * 
+     * @param scale Width and height of the window
+     */
     public Scale(int scale) {
         this.scale = scale;
     }
 
+    /**
+     * Constructs a new scale with a default size of 700x700
+     */
     public Scale() {
         this.scale = 700;
     }
 
+    /**
+     * A method to get the scale's width and height from outside.
+     * 
+     * @return the scale size
+     */
     @Override
     public int getScale() {
         return this.scale;
--- src/main/java/it/unibo/model/interfaces/ScaleInterface.java
+++ src/main/java/it/unibo/model/interfaces/ScaleInterface.java
@@ -1,5 +1,14 @@
 package it.unibo.model.interfaces;
 
+/**
+ * An interface representing the scale, aka the size of the square game window
+ * and the unit of measurement for all its elements
+ */
 public interface ScaleInterface {
+    /**
+     * A method to get the scale's width and height from outside.
+     * 
+     * @return the scale size
+     */
     int getScale();
 }
