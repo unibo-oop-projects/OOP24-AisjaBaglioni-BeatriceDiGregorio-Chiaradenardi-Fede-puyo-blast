69cb4f2b2e675102ec1fda591df6c9dcf6b85c2f
Aisja Baglioni <aisjabaglioni@gmail.com>
2025-02-05 22:31:56 +0100

    main comments

--- src/main/java/it/unibo/Main.java
+++ src/main/java/it/unibo/Main.java
@@ -11,10 +11,31 @@ import it.unibo.controller.MenuManager;
 import it.unibo.model.Scale;
 
 public class Main extends JFrame implements GameListener {
+    /**
+     * The graphics scaling of the window
+     */
     private final Scale scale;
+    /**
+     * The controller responsible for handling the Menu events
+     */
     private MenuManager menuManager;
+    /**
+     * The controller responsible for handling the Game events
+     */
     private GameManager gameManager;
 
+    /**
+     * The Main class is the entry point of the application and serves as the main
+     * window for the game. It extends JFrame and implements the GameListener
+     * interface, meaning it both creates the graphical window and responds to
+     * game-related events.
+     * 
+     * This window is square, non-resizable, and its size is dynamically set to
+     * 3/4 of the user's screen resolution.
+     * The application initially opens with the main menu.
+     * 
+     * @param title The title of the application
+     */
     public Main(String title) {
         super(title);
         Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
@@ -30,6 +51,11 @@ public class Main extends JFrame implements GameListener {
         this.menuManager.setVisible(true);
     }
 
+    /**
+     * This method removes any current game or menu managers from the frame
+     * and sets their references to null. This ensures that only one manager (either
+     * game or menu) is active at any given time.
+     */
     private void resetManager() {
         if (this.gameManager != null) {
             this.gameManager.setVisible(false);
@@ -43,24 +69,39 @@ public class Main extends JFrame implements GameListener {
         }
     }
 
+    /**
+     * the startGame method nitializes a new game manager
+     * with the appropriate level settings, adds it to the main window, and makes it
+     * visible to start the game.
+     */
     @Override
     public void startGame(GameEvent e) {
-        System.out.println("startGame");
         this.resetManager();
         this.gameManager = new GameManager(this, scale, e.levelConfig);
         this.add(this.gameManager);
         this.gameManager.setVisible(true);
     }
 
+    /**
+     * This method transitions the application back to the
+     * main menu by clearing the current view and initializing a new visible menu
+     * view.
+     */
     @Override
     public void goToMainMenu(GameEvent e) {
-        System.out.println("goToMainMenu");
         this.resetManager();
         this.menuManager = new MenuManager(this, scale);
         this.add(this.menuManager);
         this.menuManager.setVisible(true);
     }
 
+    /**
+     * The main method uses EventQueue.invokeLater() to ensure that the creation and
+     * updating of the GUI occur on the Event Dispatch Thread, which is the correct
+     * thread for Swing components.
+     * 
+     * @param args
+     */
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             Main main = new Main("Puyo Pop: Blast!");
