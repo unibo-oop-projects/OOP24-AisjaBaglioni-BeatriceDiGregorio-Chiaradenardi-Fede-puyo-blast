b13a639973f5768703bbd0b0a3a1660a3e13aaa2
Aisja Baglioni <aisjabaglioni@gmail.com>
2025-02-05 18:00:27 +0100

    point2d comments

--- src/main/java/it/unibo/model/Point2DI.java
+++ src/main/java/it/unibo/model/Point2DI.java
@@ -1,34 +1,91 @@
-//AISJA 
 
 package it.unibo.model;
 
+/**
+ * A utility record representing a 2D point with x and y integer coordinates.
+ * It provides the static utility methods for basic vector operations,
+ * an hash generator, a compare method and a Point2DI to Point2D converter.
+ */
 public record Point2DI(int x, int y) {
 
+    /**
+     * Sums two Point2DIs
+     * 
+     * @param a The first Point2DI addend
+     * @param b The second Point2DI addend
+     * 
+     * @return a Point2DI being the sum of the two parameters
+     */
     public static Point2DI add(Point2DI a, Point2DI b) {
         return new Point2DI(a.x + b.x, a.y + b.y);
     }
 
+    /**
+     * Subtracts two Point2DIs
+     * 
+     * @param a The Point2DI minuend
+     * @param b The Point2DI subtrahend
+     * 
+     * @return a Point2DI being the subtraction of the two parameters
+     */
     public static Point2DI sub(Point2DI a, Point2DI b) {
         return new Point2DI(a.x - b.x, a.y - b.y);
     }
 
+    /**
+     * Negates a Point2DI, flipping its coordinates
+     * 
+     * @param a The Point2DI to flip
+     * 
+     * @return a Point2DI being the negative of the parameter
+     */
     public static Point2DI neg(Point2DI a) {
         return new Point2DI(-a.x, -a.y);
     }
 
+    /**
+     * Multiplies a Point2DI by a scalar
+     * 
+     * @param a The Point2DI multiplicand
+     * @param b The scalar multiplier
+     * 
+     * @return a Point2D being the product of the two parameters
+     */
     public static Point2DI mul(Point2DI a, int k) {
         return new Point2DI(a.x * k, a.y * k);
     }
 
+    /**
+     * Divides a Point2DI by a scalar
+     * 
+     * @param a The Point2DI dividend
+     * @param b The scalar divisor
+     * 
+     * @return a Point2DI being the quotient of the two parameters
+     */
     public static Point2DI div(Point2DI a, int k) {
         return new Point2DI(a.x / k, a.y / k);
     }
 
+    /**
+     * Converts an integer Point2DI into a floating-point Point2D.
+     * 
+     * @param a The Point2DI to convert
+     * 
+     * @return a Point2D
+     */
     public static Point2D toPoint2D(Point2DI a) {
         return new Point2D(a.x, a.y);
     }
 
-    @Override
+    /**
+     * Checks if the parameter object is equal to the Point2DI
+     * 
+     * @param obj The object to compare
+     * 
+     * @return {@code true} if the two object have the same type and coordinates,
+     *         {@code false} otherwise.
+     */
     public boolean equals(Object obj) {
         if (this == obj) {
             return true;
@@ -40,7 +97,9 @@ public record Point2DI(int x, int y) {
         return x == other.x && y == other.y;
     }
 
-    @Override
+    /**
+     * Creates a unique integer hash code for a Point2DI, using a prime number
+     */
     public int hashCode() {
         return x * 10007 + y;
     }
