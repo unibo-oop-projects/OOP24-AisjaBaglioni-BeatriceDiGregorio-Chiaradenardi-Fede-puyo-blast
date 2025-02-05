4a48713c3f211857cfe8af338d3d321c058b991f
Aisja Baglioni <aisjabaglioni@gmail.com>
2025-02-05 23:15:53 +0100

    puyo renderer comments

--- src/main/java/it/unibo/view/PuyoRenderer.java
+++ src/main/java/it/unibo/view/PuyoRenderer.java
@@ -1,4 +1,3 @@
-//chiara & aisja
 package it.unibo.view;
 
 import it.unibo.controller.interfaces.TickListenerInterface;
@@ -14,14 +13,35 @@ import java.util.HashMap;
 import java.util.Map;
 import java.net.URL;
 
+/**
+ * This class is responsible for drawing and rendering the individual
+ * Puyos on the grid, and updates them on every tick.
+ */
 public class PuyoRenderer implements TickListenerInterface, PuyoRendererInterface {
-    // private static final int CELL_SIZE = 40; //dimensione della cella della
-    // griglia
-    private final Map<String, Integer> colorMap; // mappa per associare i colori
+    /**
+     * A map associating the color of the Puyos with the corresponding
+     * row of the spritesheet.
+     */
+    private final Map<String, Integer> colorMap;
+    /**
+     * A spritesheet
+     */
     private final Image sprites;
+    /**
+     * The graphics scaling
+     */
     private Scale scale;
+    /**
+     * The game ticks on which the animations are rendered
+     */
     private int ticks;
-    // sinistra, su, destra, giù
+    /**
+     * The last four bits of each integer determine the Puyo type to display from
+     * the spritesheet, based on 16 possible neighbor configurations.
+     * Each bit represents a direction where a neighboring Puyo is present.
+     * For example, 0101 means the Puyo has a neighbor above and below but none on
+     * the left or right.
+     */
     private static final int[] SPRITE_MAPPER = {
             0, // 0000
             1, // 0001
@@ -41,6 +61,13 @@ public class PuyoRenderer implements TickListenerInterface, PuyoRendererInterfac
             15 // 1111
     };
 
+    /**
+     * Constructs a PuyoRenderer with a given {@link Scale},
+     * initializing the ticks to zero and setting the colors and value in the
+     * colorMap.
+     * 
+     * @param scale
+     */
     public PuyoRenderer(Scale scale) {
         this.ticks = 0;
         this.scale = scale;
@@ -56,7 +83,9 @@ public class PuyoRenderer implements TickListenerInterface, PuyoRendererInterfac
         sprites = new ImageIcon(image_path).getImage();
     }
 
-    // AISJA
+    /**
+     * Checks if a Puyo in [row, col] has the same color as the given parameter
+     */
     @Override
     public int sameColorNeighbour(Grid grid, int row, int col, String color) {
         if (row < 0 || col < 0 || row >= grid.getRows() || col >= grid.getCols()) {
@@ -73,6 +102,24 @@ public class PuyoRenderer implements TickListenerInterface, PuyoRendererInterfac
         return 0;
     }
 
+    /**
+     * Displays the Puyo at [row, col] inside the {@link Grid}
+     * 
+     * The neighbor mapping is determined using the |= operator to set the
+     * rightmost bit whether a neighbor is present
+     * at the corresponding position, followed by the <<= operator
+     * to shift all bits left, adding a 0 at the rightmost bit.
+     * 
+     * If a Puyo has no neighbors, it triggers a custom animation with a probability
+     * of 2/101 every 5 ticks.
+     * 
+     * If the DeathClock is present, the Puyo briefly animates with
+     * bubbles before disappearing from the Grid.
+     * 
+     * If the FreezeClock is present, the Puyo is rendered using
+     * rows 7-13 of the spritesheet, displaying a block of ice containing
+     * the Puyo.
+     */
     @Override
     public void render(Graphics g, Grid grid, int row, int col) {
         PuyoInterface puyo = grid.getPuyo(col, row);
@@ -80,19 +127,16 @@ public class PuyoRenderer implements TickListenerInterface, PuyoRendererInterfac
             return;
         }
         int cellsize = this.scale.getScale() / 16;
-        // metà dei pezzi avanzati
         int offset_gridx = cellsize * 4;
         int offset_gridy = cellsize;
         int offset_animation = 0;
 
-        // ottieni il colore del Puyo dalla mappa
         Integer puyoRow = colorMap.get(puyo.getColor().toLowerCase());
         int x = puyo.getX() * cellsize;
         int y = puyo.getY() * cellsize;
 
         Graphics2D g2d = (Graphics2D) g;
-        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // anticrossing per
-                                                                                                  // bordi più morbidi
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         String puyoColor = puyo.getColor();
         int mask = 0;
         // null, null, null, null
@@ -118,24 +162,6 @@ public class PuyoRenderer implements TickListenerInterface, PuyoRendererInterfac
                 offset_animation = 32 * 16;
             }
         }
-        /*
-         * GradientPaint gradient = new GradientPaint(
-         * x, y, puyoColor.brighter(),
-         * x + CELL_SIZE, y + CELL_SIZE, puyoColor.darker()
-         * );
-         * 
-         * g2d.setPaint(gradient);
-         * g2d.fillOval(x + 5, y + 5, CELL_SIZE - 10, CELL_SIZE - 10);
-         * 
-         * g2d.setColor(Color.BLACK);
-         * g2d.setStroke(new BasicStroke(2));
-         * g2d.drawOval(x + 5, y + 5, CELL_SIZE - 10, CELL_SIZE - 10);
-         * 
-         * g2d.setColor(new Color(255, 255, 255, 80));
-         * g2d.fillOval(x + 10, y + 10, CELL_SIZE - 20, CELL_SIZE - 20);
-         */
-        // drawImage(Image img, int dx1, int dy1, int dx2, int dy2, int sx1, int sy1,
-        // int sx2, int sy2, ImageObserver observer)
         if (puyo.getDeathClock().isPresent()) {
             g2d.drawImage(
                     sprites,
@@ -175,6 +201,9 @@ public class PuyoRenderer implements TickListenerInterface, PuyoRendererInterfac
         }
     }
 
+    /**
+     * Increases the ticks
+     */
     @Override
     public void onTick() {
         this.ticks++;
