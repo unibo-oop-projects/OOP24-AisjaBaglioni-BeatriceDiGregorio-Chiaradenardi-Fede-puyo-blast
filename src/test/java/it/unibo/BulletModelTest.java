86c5cbc1b695f04e4d68187ebca2ee5c0ea7b93a
Aisja Baglioni <aisjabaglioni@gmail.com>
2025-02-06 01:00:48 +0100

    add bullet model test

--- src/test/java/it/unibo/BulletModelTest.java
+++ src/test/java/it/unibo/BulletModelTest.java
@@ -1,8 +1,14 @@
 package it.unibo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 
 import it.unibo.model.BulletModel;
+import it.unibo.model.Point2D;
 
 class BulletModelTest {
     private BulletModel bulletModel;
@@ -10,5 +16,30 @@ class BulletModelTest {
     @BeforeEach
     void setUp() {
         bulletModel = new BulletModel();
+    }
+
+    @Test
+    void testNotActive() {
+        assertFalse(bulletModel.isActive());
+        assertFalse(bulletModel.updatePosition());
+    }
+
+    @Test
+    void testShoot() {
+        Point2D source = new Point2D(0, 0);
+        Point2D target = new Point2D(1, 1);
+        bulletModel.shootBullet(source, target);
+        assertTrue(bulletModel.isActive());
+        assertFalse(bulletModel.targetReached());
+        for (int i = 0; i < 15; i++) {
+            assertFalse(bulletModel.targetReached());
+            assertTrue(bulletModel.updatePosition());
+        }
+        var position = bulletModel.getCurrentPosition();
+        assertEquals(position.x(), target.x());
+        assertEquals(position.y(), target.y());
+        assertFalse(bulletModel.updatePosition());
+        assertTrue(bulletModel.targetReached());
+        assertFalse(bulletModel.isActive());
     }
 }
